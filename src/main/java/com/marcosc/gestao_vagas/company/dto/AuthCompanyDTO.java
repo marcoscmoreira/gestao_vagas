85a1e49588c54766e5b8acc29a2edd1ae099c86c
Marcos <marcos_coelho2011@hotmail.com>
2023-12-15 00:18:55 +0000

    Criado Classe DTO para autenticação de company

---
+++ src/main/java/com/marcosc/gestao_vagas/company/dto/AuthCompanyDTO.java
@@ -0,0 +1,13 @@
+package com.marcosc.gestao_vagas.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthCompanyDTO {
+
+    private String password;
+    private String username;
+
+}
