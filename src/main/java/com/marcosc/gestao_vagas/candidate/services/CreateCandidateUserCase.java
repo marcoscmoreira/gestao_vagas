7a6979734c1ffceec234e2639149851273a9cd82
Marcos <marcos_coelho2011@hotmail.com>
2023-12-15 00:20:32 +0000

    Criado criptografia de senha

--- src/main/java/com/marcosc/gestao_vagas/candidate/services/CreateCandidateUserCase.java
+++ src/main/java/com/marcosc/gestao_vagas/candidate/services/CreateCandidateUserCase.java
@@ -4,6 +4,7 @@ import com.marcosc.gestao_vagas.candidate.controllers.CandidateEntity;
 import com.marcosc.gestao_vagas.candidate.controllers.CandidateRepository;
 import com.marcosc.gestao_vagas.exceptions.UserFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,20 @@ public class CreateCandidateUserCase {
 
     @Autowired
     private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute (CandidateEntity candidateEntity) {
         this.candidateRepository
                 .findByuserNameOrEmail(candidateEntity.getUserName(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        /* Realiza a criptografia da senha do candidato */
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
 
         return this.candidateRepository.save(candidateEntity);
