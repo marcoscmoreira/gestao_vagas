774eda6d352e040ad800a393c5227992faef3cd4
Marcos <marcos_coelho2011@hotmail.com>
2023-12-15 00:20:47 +0000

    Criado criptografia de senha

--- src/main/java/com/marcosc/gestao_vagas/company/services/CreateCompanyUseCase.java
+++ src/main/java/com/marcosc/gestao_vagas/company/services/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import com.marcosc.gestao_vagas.company.entities.CompanyEntity;
 import com.marcosc.gestao_vagas.company.repositories.CompanyRepository;
 import com.marcosc.gestao_vagas.exceptions.UserFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,19 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute (CompanyEntity companyEntity) {
 
         this.companyRepository.findByuserNameOrEmail(companyEntity.getUserName(), companyEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        /* Realiza a criptografia da senha da compania */
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
 
         return this.companyRepository.save(companyEntity);
 
